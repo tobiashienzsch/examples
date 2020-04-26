6a78e345e67f4813296aea3773d797a9e42a9240
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-26 04:49:11 +0200

    OpenCL: Add kernel

--- cxx_opencl/main.cpp
+++ cxx_opencl/main.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-#ifdef __APPLE__
-#include <OpenCL/cl.hpp>
-#else
 #include <CL/cl.hpp>
-#endif
 
-int main() {
+#include <iostream>
+
+int main(int, char **) {
     auto all_platforms = std::vector<cl::Platform>{};
     cl::Platform::get(&all_platforms);
     if (all_platforms.size() == 0) {
@@ -24,11 +21,46 @@ int main() {
     }
     std::cout << "Num devices: " << all_devices.size() << "\n";
 
-    auto default_device = all_devices.front();
-    std::cout << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
-    std::cout << "Using vendor: " << default_device.getInfo<CL_DEVICE_VENDOR>() << "\n";
-    std::cout << "Using version: " << default_device.getInfo<CL_DEVICE_VERSION>() << "\n";
+    auto device = all_devices.front();
+    std::cout << "Using device: " << device.getInfo<CL_DEVICE_NAME>() << "\n";
+    std::cout << "Using vendor: " << device.getInfo<CL_DEVICE_VENDOR>() << "\n";
+    std::cout << "Using version: " << device.getInfo<CL_DEVICE_VERSION>() << "\n";
 
+    auto source = std::string{R"(
+        __kernel void HelloWorld(__global char* data) {
+            data[0] = 'H';
+            data[1] = 'e';
+            data[2] = 'l';
+            data[3] = 'l';
+            data[4] = 'o';
+            data[5] = ' ';
+            data[6] = 'W';
+            data[7] = 'o';
+            data[8] = 'r';
+            data[9] = 'l';
+            data[10] = 'd';
+        }
+    )"};
+
+    auto sources = cl::Program::Sources{1, std::make_pair(source.c_str(), source.length())};
+    auto context = cl::Context{device};
+    auto program = cl::Program{context, sources};
+
+    auto err = program.build();
+    if (err != CL_SUCCESS) {
+        std::cerr << "Error: Building Program Code: " << err << "\n";
+    }
+
+    char buf[16] = {};
+    auto memBuffer = cl::Buffer{context, CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY, sizeof(buf)};
+    auto kernel = cl::Kernel{program, "HelloWorld", &err};
+    kernel.setArg(0, memBuffer);
+
+    auto queue = cl::CommandQueue{context, device};
+    queue.enqueueTask(kernel);
+    queue.enqueueReadBuffer(memBuffer, CL_TRUE, 0, sizeof(buf), buf);
+
+    std::cout << "Buffer: " << buf << "\n";
 
     return EXIT_SUCCESS;
 }
