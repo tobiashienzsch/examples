4cfc585539e1fc2a6fa3be4594a5535cfe153368
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-05-07 05:47:14 +0200

    Update boost ipc example

--- cxx_boost_ipc/main.cpp
+++ cxx_boost_ipc/main.cpp
@@ -4,7 +4,8 @@
 
 using namespace boost::interprocess;
 
-int main() {
+int server() {
+  std::cout << "server" << '\n';
   try {
     // Erase previous message queue
     message_queue::remove("message_queue");
@@ -18,8 +19,48 @@ int main() {
     }
   } catch (interprocess_exception &ex) {
     std::cout << ex.what() << std::endl;
-    return 1;
+    return EXIT_FAILURE;
   }
 
   return 0;
+}
+
+int client() {
+  std::cout << "client" << '\n';
+
+  try {
+    // Open a message queue.
+    message_queue mq(open_only, "message_queue");
+
+    unsigned int priority;
+    message_queue::size_type recvd_size;
+
+    // Receive 100 numbers
+    for (int i = 0; i < 100; ++i) {
+      int number;
+      mq.receive(&number, sizeof(number), recvd_size, priority);
+      if (number != i || recvd_size != sizeof(number))
+        return EXIT_FAILURE;
+      std::cout << std::to_string(number) << '\n';
+    }
+  } catch (interprocess_exception &ex) {
+    message_queue::remove("message_queue");
+    std::cout << ex.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  message_queue::remove("message_queue");
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+  if (argc == 2) {
+    if (std::string(argv[1]) == "client") {
+      return client();
+    }
+    if (std::string(argv[1]) == "server") {
+      return server();
+    }
+  }
+
+  return EXIT_SUCCESS;
 }
