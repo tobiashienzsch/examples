82d72f81361431dc3e811e0cfc623dc72b0949eb
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-03-02 07:41:09 +0100

    Update cxx_sycl

--- cxx_mna/README.md
+++ cxx_mna/README.md
@@ -7,3 +7,10 @@
 - [SPICE ‘Quick’ Reference Sheet](https://web.stanford.edu/class/ee133/handouts/general/spice_ref.pdf)
 - [A Short SPICE Tutorial](https://www.egr.msu.edu/classes/ece445/mason/Files/KSU_short_spice_tutorial.pdf)
 - [A GENERALIZED METHOD FOR THE DERIVATION OF NON-LINEAR STATE-SPACE MODELS FROM CIRCUIT SCHEMATICS](https://www.hsu-hh.de/ant/wp-content/uploads/sites/699/2017/10/Holters-Z%C3%B6lzer-2015-A-Generalized-Method-for-the-Derivation-of-Non-linear-State-space-Models-from-Circuit-Schematics.pdf)
+
+- https://www.youtube.com/watch?v=lMk97dWHs-E
+- https://www.youtube.com/watch?v=BPapfxzwsmI&list=PLlsTHbFeR7v0QG8Q4l6oc7d---eKKSKTz&index=1
+- https://www.youtube.com/watch?v=x6Eps_QP5qM&list=PLX2gX-ftPVXWv1eWntPcZtztrmwYBiBQY&index=3
+- https://en.wikiversity.org/wiki/Electric_Circuit_Analysis/Nodal_Analysis
+- https://johnloomis.org/ece595a/notes/nodal/nodal.html
+- http://lpsa.swarthmore.edu/Systems/Electrical/mna/MNA1.html
--- cxx_sycl/main.cpp
+++ cxx_sycl/main.cpp
@@ -9,8 +9,7 @@
 
 auto toString(sycl::info::device_type type) -> std::string
 {
-    switch (type)
-    {
+    switch (type) {
         case sycl::info::device_type::cpu: return "CPU";
         case sycl::info::device_type::gpu: return "GPU";
         case sycl::info::device_type::accelerator: return "Accelerator";
@@ -35,20 +34,18 @@ auto printDeviceInfo(sycl::device dev) -> void
     std::printf("Vendor: %s\n", vendor.c_str());
     std::printf("Type: %s\n", toString(type).c_str());
     std::printf("Max alloc size: %zu MB\n", maxAllocSize / 1024 / 1024);
-    for (auto groupSize : dev.get_info<sycl::info::device::sub_group_sizes>())
-    {
+    for (auto groupSize : dev.get_info<sycl::info::device::sub_group_sizes>()) {
         std::printf("Subgroup size: %zu\n", groupSize);
     }
     std::printf("\n");
 }
 
-struct UsmDeviceSelector : sycl::device_selector
+struct UsmDeviceSelector
 {
     auto operator()(sycl::device const& dev) const -> int
     {
-        if (dev.has(sycl::aspect::usm_device_allocations))
-        {
-            if (dev.has(sycl::aspect::gpu)) return 2;
+        if (dev.has(sycl::aspect::usm_device_allocations)) {
+            if (dev.has(sycl::aspect::gpu)) { return 2; }
             return 1;
         }
         return -1;
@@ -59,29 +56,19 @@ class hello_world;
 
 auto main() -> int
 {
-    for (auto dev : cl::sycl::device::get_devices())
-    {
-        printDeviceInfo(dev);
-    }
+    for (auto dev : sycl::device::get_devices()) { printDeviceInfo(dev); }
 
-    auto queue  = sycl::queue {UsmDeviceSelector {}};
+    auto queue  = sycl::queue{UsmDeviceSelector{}};
     auto device = queue.get_device();
     printDeviceInfo(device);
 
-    try
-    {
-        queue
-            .submit(
-                [&](sycl::handler& cgh)
-                {
-                    auto os = sycl::stream {128, 128, cgh};
-                    cgh.single_task<hello_world>([=]()
-                                                 { os << "Hello World!\n"; });
-                })
-            .wait();
-    }
-    catch (std::exception const& e)
-    {
+    try {
+        auto kernel = [&](sycl::handler& cgh) {
+            auto os = sycl::stream{128, 128, cgh};
+            cgh.single_task<hello_world>([=]() { os << "Hello World!\n"; });
+        };
+        queue.submit(kernel).wait();
+    } catch (std::exception const& e) {
         std::fprintf(stderr, "Exception caught: %s\n", e.what());
         return EXIT_FAILURE;
     }
